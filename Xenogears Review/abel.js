4250e7e92b31f36f4590f9518f6f50b1b743ae29
KristopherManalo <kristophermmanalo@gmail.com>
2022-10-13 11:14:26 -0700

    Added Xenogears review

---
+++ Xenogears Review/abel.js
@@ -0,0 +1,20 @@
+/*
+topbutton = document.getElementById("topBtn");
+
+window.onscroll = function() {scrollfunction()};
+
+function scrollFunction(){
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+		topbutton.style.display = "block";
+	}
+	else
+	{
+		topbutton.style.display = "none";
+	}
+}
+
+function topPage() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+*/
